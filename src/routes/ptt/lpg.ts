7853b3b5f85e1714afce8566e74081eff9d29897
Tirawat Owen <bamiowen@gmail.com>
2025-11-24 23:11:22 +0700

    complete lpg fuel ratio

---
+++ src/routes/ptt/lpg.ts
@@ -0,0 +1,38 @@
+import { Effect } from "effect";
+import Elysia, { t } from "elysia";
+import { ExtractPDFService } from "../../extract-pdf.service";
+import { elysiaPdf } from "../../helpers";
+import { Runtime } from "../../runtime";
+import { pttLpgSchemaAndPrompt } from "../../schema/ptt/lpg";
+
+export const lpgRoutes = new Elysia().group("/lpg", (c) =>
+  c.post(
+    "/fuel-ratio",
+    async ({ body }) => {
+      const file = body.file;
+      const arrBuf = await file.arrayBuffer();
+      const buf = Buffer.from(arrBuf);
+
+      const result = await Effect.all({
+        svc: ExtractPDFService,
+      }).pipe(
+        Effect.andThen(({ svc }) =>
+          svc.processInline(
+            buf,
+            pttLpgSchemaAndPrompt.fuelRatio.systemPrompt,
+            pttLpgSchemaAndPrompt.fuelRatio.schema
+          )
+        ),
+        Runtime.runPromise
+      );
+
+      return result;
+    },
+    {
+      body: t.Object({
+        file: elysiaPdf,
+      }),
+      tags: ["PTT"],
+    }
+  )
+);
